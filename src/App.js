56ea2891d2b5f2916a36da5249ce5d64c6070672
Rishu Gupta <guptarishu04@gmail.com>
2024-05-26 20:28:28 +0530

    notes data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Notes from './Notes';
 
 function App() {
+  const [notes, setNotes] = useState([
+    {
+      id:1,
+      text:'Complete React Coding question'
+    },
+    {
+      id:2,
+      text:'Leetcode easy question done'
+    }
+  ])
   return (
     <div className="App">
-     
+     <Notes notes={notes} setNotes={setNotes}/>
     </div>
   );
 }
